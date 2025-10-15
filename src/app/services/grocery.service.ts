c92d3b68847eb4262e2ac32b981b88a2e140ff3c
Bryson Smith <morrianton@gmail.com>
2025-10-14 18:29:11 -0600

    Configure API endpoint for dynamic setting.

--- src/app/services/grocery.service.ts
+++ src/app/services/grocery.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryService {
-  private apiUrl = '/api/grocery-items';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://grocery-calculator-yvsk.onrender.com/api/grocery-items'
 };
